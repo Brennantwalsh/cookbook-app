3a779bd3fa4dfdda411d2892da646a2c5d71cab8
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-07 20:49:51 -0500

    updated

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(' ')
+  end
+
+  def directions_list
+    directions.split(', ')
+  end
+
+  def friendly_updated_at
+    updated_at.strftime('%b %d, %Y')
+  end
+
+  def friendly_prep_time
+    message = ""
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    message += "#{hours} #{'hour'.pluralize(hours)}" if hours > 0
+    message += " and " if hours > 0 && minutes > 0
+    message += " #{minutes} #{'minute'.pluralize(minutes)}" if minutes
+  end
 end
